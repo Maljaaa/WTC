cdac6b85a49a66c920d38f552e457ef7092375d7
Maljaaa <Maljaaa@github.com>
2023-11-15 14:50:26 +0900

    feat(EventService): 종류별 메뉴 주문 수량 체크 기능 구현
    - setMenuResult() : 메뉴가 어떤 종류냐에 따라서 MenuResult 값 증가

--- src/main/java/christmas/service/EventService.java
+++ src/main/java/christmas/service/EventService.java
@@ -1,16 +1,59 @@
 package christmas.service;
 
+import christmas.model.menu.MenuResult;
+
 import java.util.Arrays;
 import java.util.List;
 
+import static christmas.model.menu.MenuName.*;
 import static java.util.stream.Collectors.*;
 
 public class EventService {
 
     public List<String> getMenuName(String order) {
-        return Arrays.stream(order.split(","))
+
+        List<String> menu = Arrays.stream(order.split(","))
                 .map(s -> s.substring(0, s.indexOf('-')))
                 .collect(toList());
+
+        setMenuResult(menu);
+
+        return menu;
+    }
+
+    private void setMenuResult(List<String> menu) {
+        MenuResult menuResult = new MenuResult();
+
+        for (String food : menu) {
+            isAppetizer(menuResult, food);
+            isBeverage(menuResult, food);
+            isDesert(menuResult, food);
+            isMain(menuResult, food);
+        }
+    }
+
+    private void isMain(MenuResult menuResult, String food) {
+        if (food.equals(BARBECUED_RIBS.getMenu()) || food.equals(CHRISTMAS_PASTA.getMenu()) || food.equals(SEAFOOD_PASTA.getMenu()) || food.equals(T_BONE_STEAK.getMenu())) {
+            menuResult.addMainCount();
+        }
+    }
+
+    private void isDesert(MenuResult menuResult, String food) {
+        if (food.equals(CHOCO_CAKE.getMenu()) || food.equals(ICECREAM.getMenu())) {
+            menuResult.addDesertCount();
+        }
+    }
+
+    private void isBeverage(MenuResult menuResult, String food) {
+        if (food.equals(CHAMPAGNE.getMenu()) || food.equals(COKE_ZERO.getMenu()) || food.equals(RED_WINE.getMenu())) {
+            menuResult.addBeverageCount();
+        }
+    }
+
+    private void isAppetizer(MenuResult menuResult, String food) {
+        if (food.equals(CAESAR_SALAD.getMenu()) || food.equals(MUSHROOM_SOUP.getMenu()) || food.equals(TAPAS.getMenu())) {
+            menuResult.addAppetizerCount();
+        }
     }
 
     public List<Integer> getMenuCount(String order) {
@@ -19,4 +62,5 @@ public class EventService {
                 .map(Integer::parseInt)
                 .collect(toList());
     }
+
 }
