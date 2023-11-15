76a4dc8d45a96d0f95c871d901f49659f288e26d
Maljaaa <Maljaaa@github.com>
2023-11-15 23:52:39 +0900

    test(OrderService): 주문 테스트
    - 주문 후 전체 가격 확인 테스트

--- src/test/java/christmas/service/OrderServiceTest.java
+++ src/test/java/christmas/service/OrderServiceTest.java
@@ -2,11 +2,13 @@ package christmas.service;
 
 import christmas.model.menu.MenuAllPrice;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static christmas.model.menu.utils.MenuPrice.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
@@ -17,14 +19,17 @@ class OrderServiceTest {
         orderService = new OrderService();
     }
 
+    @DisplayName("주문 후 전체 가격 확인 테스트")
     @Test
     public void allPriceTest() throws Exception {
         List<String> menu = Arrays.asList("티본스테이크", "바비큐립", "초코케이크", "제로콜라");
         List<Integer> menuCount = Arrays.asList(1, 1, 2, 1);
         orderService.orderMenu(menu, menuCount);
 
-        // 여기서 실제 메뉴의 가격을 설정해야 합니다.
-        int expectedAllPrice = 55000 + 54000 + 30000 + 3000; // 예시 가격
+        int expectedAllPrice = T_BONE_STEAK_PRICE.getPrice() +
+                BARBECUED_RIBS_PRICE.getPrice() +
+                (CHOCO_CAKE_PRICE.getPrice() * 2) +
+                COKE_ZERO_PRICE.getPrice();
         assertEquals(expectedAllPrice, MenuAllPrice.getInstance().getAllPrice());
     } 
 
