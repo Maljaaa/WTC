8c03aee2f160aa205585e991ce9e778a65d2ce80
Maljaaa <Maljaaa@github.com>
2023-11-15 21:32:53 +0900

    refactor(InputView): 잘못된 주문을 입력받았을 경우 예외처리
    - 잘못된 주문을 입력받았을 경우 IllegalArgumentException을 발생시키고 다시 입력받음
    - IllegalArgumentException과 StringIndexOutOfBoundsException을 예외처리함

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,11 +1,23 @@
 package christmas.view;
 
+import christmas.model.menu.MenuName;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static christmas.view.Error.*;
 import static christmas.view.Format.*;
 import static christmas.view.Show.*;
+import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.toList;
 
 public class InputView {
+
+    private Set<String> availableMenus = Arrays.stream(MenuName.values())
+            .map(MenuName::getMenu)
+            .collect(toSet());
 
     public String readDate() {
         while (true) {
@@ -27,8 +39,47 @@ public class InputView {
     }
 
     public String readOrder() {
-        System.out.println(READ_ORDER.getMessage());
+        while (true) {
+            try {
+                System.out.println(READ_ORDER.getMessage());
+                String order = readLine();
+                validateOrder(order);
+                validateOrderCount(order);
+                return order;
+            } catch (IllegalArgumentException | StringIndexOutOfBoundsException e) {
+                System.out.println(INVALID_ORDER.getMessage());
+            }
+        }
+    }
 
-        return readLine();
+    private void validateOrder(String order) {
+        List<String> orderedMenus = getMenuName(order);
+        for (String menu : orderedMenus) {
+            if (!availableMenus.contains(menu)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public List<String> getMenuName(String order) {
+        return Arrays.stream(order.split(","))
+                .map(s -> s.substring(0, s.indexOf('-')))
+                .collect(toList());
+    }
+
+    private void validateOrderCount(String order) {
+        List<Integer> orderMenusCount = getMenuCount(order);
+        for (Integer orderCount : orderMenusCount) {
+            if (orderCount <= 0) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public List<Integer> getMenuCount(String order) {
+        return Arrays.stream(order.split(","))
+                .map(s -> s.substring(s.indexOf('-') + 1))
+                .map(Integer::parseInt)
+                .collect(toList());
     }
 }
