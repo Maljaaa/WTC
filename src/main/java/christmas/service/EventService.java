9584abd1fdccbc115f5032ade3423486a69cebde
Maljaaa <Maljaaa@github.com>
2023-11-15 14:12:57 +0900

    feat(EventService): 메뉴 분석 기능 구현
    - String order로 받은 내용으로 getMenuName()에서 주문 받은 메뉴 이름들을 구분하여 반환하고 getMenuCount()에서 주문 수량을 구분하여 반환한다.

---
+++ src/main/java/christmas/service/EventService.java
@@ -0,0 +1,22 @@
+package christmas.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.*;
+
+public class EventService {
+
+    public List<String> getMenuName(String order) {
+        return Arrays.stream(order.split(","))
+                .map(s -> s.substring(0, s.indexOf('-')))
+                .collect(toList());
+    }
+
+    public List<Integer> getMenuCount(String order) {
+        return Arrays.stream(order.split(","))
+                .map(s -> s.substring(s.indexOf('-') + 1))
+                .map(Integer::parseInt)
+                .collect(toList());
+    }
+}
