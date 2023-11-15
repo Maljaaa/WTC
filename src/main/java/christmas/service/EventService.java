2de0de5c09dc2b1752032b19732d399eb4ca66d7
Maljaaa <Maljaaa@github.com>
2023-11-15 14:54:08 +0900

    style(EventService): 코드 변화 없음
    - MenuResult -> MenuCount

--- src/main/java/christmas/service/EventService.java
+++ src/main/java/christmas/service/EventService.java
@@ -1,6 +1,6 @@
 package christmas.service;
 
-import christmas.model.menu.MenuResult;
+import christmas.model.menu.MenuCount;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,37 +22,37 @@ public class EventService {
     }
 
     private void setMenuResult(List<String> menu) {
-        MenuResult menuResult = new MenuResult();
+        MenuCount menuCount = new MenuCount();
 
         for (String food : menu) {
-            isAppetizer(menuResult, food);
-            isBeverage(menuResult, food);
-            isDesert(menuResult, food);
-            isMain(menuResult, food);
+            isAppetizer(menuCount, food);
+            isBeverage(menuCount, food);
+            isDesert(menuCount, food);
+            isMain(menuCount, food);
         }
     }
 
-    private void isMain(MenuResult menuResult, String food) {
+    private void isMain(MenuCount menuCount, String food) {
         if (food.equals(BARBECUED_RIBS.getMenu()) || food.equals(CHRISTMAS_PASTA.getMenu()) || food.equals(SEAFOOD_PASTA.getMenu()) || food.equals(T_BONE_STEAK.getMenu())) {
-            menuResult.addMainCount();
+            menuCount.addMainCount();
         }
     }
 
-    private void isDesert(MenuResult menuResult, String food) {
+    private void isDesert(MenuCount menuCount, String food) {
         if (food.equals(CHOCO_CAKE.getMenu()) || food.equals(ICECREAM.getMenu())) {
-            menuResult.addDesertCount();
+            menuCount.addDesertCount();
         }
     }
 
-    private void isBeverage(MenuResult menuResult, String food) {
+    private void isBeverage(MenuCount menuCount, String food) {
         if (food.equals(CHAMPAGNE.getMenu()) || food.equals(COKE_ZERO.getMenu()) || food.equals(RED_WINE.getMenu())) {
-            menuResult.addBeverageCount();
+            menuCount.addBeverageCount();
         }
     }
 
-    private void isAppetizer(MenuResult menuResult, String food) {
+    private void isAppetizer(MenuCount menuCount, String food) {
         if (food.equals(CAESAR_SALAD.getMenu()) || food.equals(MUSHROOM_SOUP.getMenu()) || food.equals(TAPAS.getMenu())) {
-            menuResult.addAppetizerCount();
+            menuCount.addAppetizerCount();
         }
     }
 
